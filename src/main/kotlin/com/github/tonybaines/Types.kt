0d1e5d0e1dc2be59c8ac61326d8fcfcfe41d8d50
Tony Baines <tony.baines.1@gmail.com>
2020-07-27 14:27:43 +0100

    Start of a major re-working. Begin with working through the types

--- src/main/kotlin/com/github/tonybaines/Types.kt
+++ src/main/kotlin/com/github/tonybaines/Types.kt
@@ -1,12 +1,40 @@
 package com.github.tonybaines
 
-import arrow.fx.IO
 
-enum class Method {GET, PUT, POST, DELETE}
+enum class Method { GET, PUT, POST, DELETE }
 
 data class HttpRequest(val method: Method, val uri: String)
 
 data class HttpResponse(val request: HttpRequest, val status: Int = 200, val body: String)
 
-typealias ResponseProvider = IO<Result>
-typealias Result = Pair<Int, String>
+typealias ResponseProvider = (HttpRequest) -> HttpResponse
+
+sealed class MaybeValid {
+    class Valid(var request: HttpRequest) : MaybeValid()
+    sealed class Invalid(val status: Int, var reason: String) : MaybeValid() {
+        class NotFound() : Invalid(404,"Not Found")
+        class BadRequest() : Invalid(400,"Bad Request")
+    }
+}
+
+/**
+ * Something went wrong
+ */
+sealed class Outcome {
+    sealed class Good(val status: Int, val body: String) : Outcome() {}
+
+    sealed class Bad : Outcome() {
+        abstract val reason: String
+
+        class JustBad(var status: Int, val message: String) : Bad() {
+            override val reason: String
+                get() = message
+
+        }
+
+        class ExceptionallyBad(var status: Int, val e: Exception) : Bad() {
+            override val reason: String
+                get() = e.localizedMessage
+        }
+    }
+}
--- src/main/kotlin/com/github/tonybaines/WebApp.kt
+++ src/main/kotlin/com/github/tonybaines/WebApp.kt
@@ -1,42 +1,48 @@
 package com.github.tonybaines
 
-import arrow.core.getOrElse
-import arrow.fx.IO
-import arrow.fx.typeclasses.Duration
-import arrow.fx.typeclasses.seconds
-
-
 object WebApp {
 
-    private val someValidPath: ResponseProvider = IO.just(Result(200, "Hello World!"))
-    private val anotherValidPath: ResponseProvider = IO.just(Result(200, "42"))
-    private val brokenPath: ResponseProvider = IO.raiseError(RuntimeException("Whoops!"))
-    private val notFound: ResponseProvider = IO.just(Result(404, ""))
+    private val someValidPath: ResponseProvider = { req -> HttpResponse(req,200, "Hello World!") }
+    private val anotherValidPath: ResponseProvider = { req -> HttpResponse(req,200, "42") }
+//    private val brokenPath: ResponseProvider = { ItFailed(BadThing.ExceptionallyBadThing(RuntimeException("Whoops!"))) }
 
-    private fun ResponseProvider.delayedBy(seconds: Duration): ResponseProvider =
-        IO.sleep(seconds)
-            .followedBy(this)
+//    private val notFound: ResponseProvider = { ItWorked(Result.Yup(404, "")) }
+
+//    private fun ResponseProvider.delayedBy(seconds: Duration): ResponseProvider =
+//        IO.sleep(seconds)
+//            .followedBy(this)
 
 
-    private fun providerFor(req: HttpRequest): ResponseProvider =
-        when (req.uri) {
-            "/some/valid/path" -> someValidPath
-            "/another/valid/path" -> anotherValidPath
-            "/broken/path" -> brokenPath
-            "/resource/which/takes/1s/to/complete" -> anotherValidPath.delayedBy(1.seconds)
-            "/resource/which/takes/3s/to/complete" -> anotherValidPath.delayedBy(3.seconds)
-            else -> notFound
-        }
+    private fun providerFor(req: HttpRequest): ResponseProvider = someValidPath
+//        when (req.uri) {
+//            "/some/valid/path" -> someValidPath
+//            "/another/valid/path" -> anotherValidPath
+//            "/broken/path" -> brokenPath
+//            "/resource/which/takes/1s/to/complete" -> anotherValidPath//.delayedBy(1.seconds)
+//            "/resource/which/takes/3s/to/complete" -> anotherValidPath//.delayedBy(3.seconds)
+//            else -> notFound
+//        }
 
 
     fun handle(httpRequest: HttpRequest): HttpResponse =
-        providerFor(httpRequest)
-            .redeem(
-                { t -> HttpResponse(httpRequest, 500, t.localizedMessage) },
-                { result -> HttpResponse(httpRequest, result.first, result.second) }
-            )
-            .unsafeRunTimed(2.seconds)
-            .getOrElse { HttpResponse(httpRequest, 503, "Processing timed-out") }
-
+        providerFor(httpRequest)(httpRequest)
 
 }
+
+//private fun ResponseProvider.handleTheResult(
+//    whenAGoodThingHappens: (ItWorked) -> HttpResponse,
+//    whenABadThingHappens: (ItFailed) -> HttpResponse): ResponseProvider = {
+//
+//    when() {
+//        is ItFailed -> { thing: ItFailed -> whenABadThingHappens(thing) }
+//        is ItWorked -> { thing: ItWorked -> whenAGoodThingHappens(thing) }
+//    }
+//}
+//
+//private fun ResponseProvider.getOrElse(defaultError: () -> HttpResponse): HttpResponse {
+//    val result = this()
+//    return when (result) {
+//        is ItWorked -> result.result
+//        is ItFailed -> defaultError()
+//    }
+//}
